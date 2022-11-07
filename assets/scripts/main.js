45fe565621c53221b0b60d89f76400b62b5bdfdc
nathanenrici <nenrici@ucsd.edu>
2022-11-06 18:31:04 -0800

    aqebtrnhoik[jb

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,6 +7,7 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // Get the recipes from localStorage
   let recipes = getRecipesFromStorage();
+  console.log(recipes[1]);
   // Add each recipe to the <main> element
   addRecipesToDocument(recipes);
   // Add the event listeners to the form elements
@@ -24,6 +25,8 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  return JSON.parse(localStorage.getItem('recipes'));
+  
 }
 
 /**
@@ -35,10 +38,17 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let main = document.querySelector("main");
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  length = recipes.length;
+  for (let i = 0; i < length; i++){
+    let xlr8 = document.createElement("recipe-card");
+    xlr8.data = (recipes[i]);
+    main.append(xlr8);
+  }
 }
 
 /**
